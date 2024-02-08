ecee66715f6c66a26d5a6cc320756fb6b19317dd
troykerim <troykerim@gmail.com>
2024-02-08 11:26:00 -0800

    Adding fibonnaci V1

--- decimal2binary/decimal2binary.c
+++ decimal2binary/decimal2binary.c
@@ -25,13 +25,17 @@ int main()
 
 void dec2bin(uint8_t num)
 {
-    int bin_num[8] = {0};  // Array to store all binary digits
+    int bin_num[8];  // Array to store all binary digits
 
+    // Initializing the array.
+    for(int i = 0; i < 8; i++)
+    {
+        bin_num[i] = 0;
+    }
+
+    // Load data into bin_num array.
     int i = 0;
-
-    // Load data into bin_num array.  
-    // Note: can also do bin_num[8] = {0,0,0,0,0,0,0,0} to remove while loop
-    while(num > 0)
+    while(num > 0)  
     {
         bin_num[i] = num % 2; // Converts a decimal value to binary
         num = num / 2;
@@ -39,10 +43,10 @@ void dec2bin(uint8_t num)
     }
 
     int bit = i;
-    for(int j = bit - 1; j >= 0; j--)
+    for(int j = 7; j >= 0; j--) // Start at 7 downto 0
     {
         printf("%d", bin_num[j]);
-        if((bit - j) % 4 == 0 && j != 0)
+        if((7 - j + 1) % 4 == 0 && j != 0) // Check the position of the bit from right most side
         {
             printf("_");
         }
--- fibonnaci/fibonnaci.c
+++ fibonnaci/fibonnaci.c
@@ -1,7 +1,58 @@
+/*
+    Interation  Output number:
+    n = 1,      fib = 0
+    n = 2,      fib = 1
+    n = 3,      fib = 1
+    n = 4,      fib = 2
+    n = 5,      fib = 3
+
+*/
 #include <stdio.h>
+
+void printFib(int num);
 
 int main()
 {
-    printf("Hello World!\n");
+    int num; 
+    printf("Total sum for a Fibonnaci sequence.\n");
+    printf("Enter size: ");
+    scanf("%d", &num);
+
+    if(num < 0)
+    {
+        printf("Please enter a non-negative integer.\n");
+        return 1;
+    }
+    else{
+        printFib(num);
+    }
+    // printf("The total sum for the fibonnaci seequence of %d is %d \n",num, fib(num));
+
     return 0;
 }
+
+void printFib(int num)
+{
+    int a = 0, b = 1, next;
+
+    printf("Fibonnaci sequence up to term %d is: ", num);
+    for(int i = 0; i<num; i++)
+    {
+        if(i == 0)
+        {
+            printf("%d", a);
+        }
+        else if(i == 1)
+        {
+            printf(", %d", b);
+        }
+        else{
+            next = a + b;
+            a = b;
+            b = next;
+            printf(", %d", next);
+        }
+    }
+    printf("\n");
+}
+
