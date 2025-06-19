e371ffd73860010d434162349ea21f86b2b1f896
troykerim <troykerim@gmail.com>
2025-06-18 22:01:48 -0700

    Adding pyramid

---
+++ pyramid/build/CMakeFiles/pyramid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pyramid.dir/pyramid.c.o"
+  "CMakeFiles/pyramid.dir/pyramid.c.o.d"
+  "pyramid"
+  "pyramid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pyramid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
