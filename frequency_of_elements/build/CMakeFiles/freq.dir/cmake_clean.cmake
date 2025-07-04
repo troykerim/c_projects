41644c2b22b32c6c1cde1c38c1cf42bc403f85e8
troykerim <troykerim@gmail.com>
2025-07-03 22:51:40 -0700

    Adding frequency of elements in array

---
+++ frequency_of_elements/build/CMakeFiles/freq.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freq.dir/freq.c.o"
+  "CMakeFiles/freq.dir/freq.c.o.d"
+  "freq"
+  "freq.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/freq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
