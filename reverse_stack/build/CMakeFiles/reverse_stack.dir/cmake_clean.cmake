06a64fa06dfea41b5481c6e5d6d9e7f66412d351
troykerim <troykerim@gmail.com>
2025-07-14 22:15:49 -0700

    Adding project

---
+++ reverse_stack/build/CMakeFiles/reverse_stack.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverse_stack.dir/reverse_stack.c.o"
+  "CMakeFiles/reverse_stack.dir/reverse_stack.c.o.d"
+  "reverse_stack"
+  "reverse_stack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/reverse_stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
